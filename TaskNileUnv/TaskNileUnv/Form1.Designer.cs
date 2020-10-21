3df013b650c759f28a6ffff4f9b409e9cce63b14
salmahassan7979 <48031643+salmahassan7979@users.noreply.github.com>
2020-10-22 00:50:09 +0200

    Add files via upload

---
+++ TaskNileUnv/TaskNileUnv/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace TaskNileUnv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Send = new System.Windows.Forms.Button();
+            this.Receive = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(381, 72);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(285, 47);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(369, 207);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(273, 50);
+            this.textBox2.TabIndex = 1;
+            // 
+            // Send
+            // 
+            this.Send.Location = new System.Drawing.Point(163, 72);
+            this.Send.Name = "Send";
+            this.Send.Size = new System.Drawing.Size(106, 47);
+            this.Send.TabIndex = 2;
+            this.Send.Text = "Send";
+            this.Send.UseVisualStyleBackColor = true;
+            this.Send.Click += new System.EventHandler(this.Send_Click);
+            // 
+            // Receive
+            // 
+            this.Receive.Location = new System.Drawing.Point(163, 207);
+            this.Receive.Name = "Receive";
+            this.Receive.Size = new System.Drawing.Size(106, 50);
+            this.Receive.TabIndex = 3;
+            this.Receive.Text = "Receive";
+            this.Receive.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(163, 308);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(183, 28);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(428, 316);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Disconnect";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(348, 386);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 40);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Receive);
+            this.Controls.Add(this.Send);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button Send;
+        private System.Windows.Forms.Button Receive;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TaskNileUnv/TaskNileUnv/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace TaskNileUnv
+{
+    public partial class Form1 : Form
+    {
+        SerialPort port = null;
+        String data_rx = " ";
+        int data_tx = 60;
+        int num;
+        bool flag_rx = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Send_Click(object sender, EventArgs e)
+        {
+            
+            send();
+        }
+        private void send()
+        {
+            try
+            {
+                port.Write("@" + textBox1.Text + ";");
+                data_tx = Int32.Parse(textBox1.Text);
+
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void TextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                send();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connect();
+        }
+        private void connect()
+        {
+            port = new SerialPort(comboBox1.SelectedItem.ToString());
+            port.DataReceived += new SerialDataReceivedEventHandler(data_rx_handler);
+            port.BaudRate = 9600;
+            port.DataBits = 8;
+            port.StopBits = StopBits.One;
+            try
+            {
+                if (!port.IsOpen)
+                {
+                    port.Open();
+                    label1.Text = "Connected";
+                    label1.ForeColor = Color.Green;
+
+                }
+            }
+            catch (Exception e)
+            {
+            }
+        }
+        private void data_rx_handler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort port1 = (SerialPort)sender;
+            String text = port1.ReadExisting();
+            int idx_end = text.IndexOf(';');
+            if ((idx_end >= 0) && flag_rx)
+            {
+                flag_rx = false;
+                try
+                {
+                    data_rx += text.Substring(0, idx_end);
+                   
+                }
+                catch (Exception m)
+                {
+                    data_rx = m.Message;
+                }
+            }
+            if (flag_rx)
+            {
+                data_rx += text;
+            }
+            int idx_start = text.IndexOf('@');
+            if (idx_start >= 0)
+            {
+
+                flag_rx = true;
+                data_rx = "";
+                if (text.Length > (idx_start + 1))
+                {
+                    data_rx += text.Substring((idx_start + 1), (text.Length - 1));
+                    int idx = data_rx.IndexOf(';');
+                    if (idx >= 0)
+                    {
+                        data_rx = data_rx.Substring(0, idx);
+                    }
+                }
+            }
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
